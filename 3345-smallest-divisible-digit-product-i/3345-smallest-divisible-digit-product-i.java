2087a1f46d771bc3024f061b4fd7d8a7158421a6
Aadarsh Babu <145755350+Aadymanda@users.noreply.github.com>
2026-08-06 17:49:22 +0530

    Time: 1 ms (100%), Space: 42.5 MB (82.46%) - LeetHub

--- 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
+++ 3345-smallest-divisible-digit-product-i/3345-smallest-divisible-digit-product-i.java
@@ -1,32 +1,21 @@
 class Solution {
     public int smallestNumber(int n, int t) {
-        if(n==1){
-            return t;
-        }
-        
-       
-        int num=n;
-        int ans=1;
-        int sol=-1;
-        while(sol!=0){
-            ans=1;
-        
-            while(n>0){
-                int rem=n%10;
+        int a=((n/10)+1)*10;
+        for(int i=n;i<=a;i++){
+            int j=i;
+            int ans=1;
+            while(j>0){
+                int rem=j%10;
                 ans=ans*rem;
-                n=n/10;
-                
-            }
-            sol=ans%t;
-            if(sol!=0){
-                num++;
-            }
-            n=num;
-       
-            
-        }
-    
-        return num;
-    }
+                j=j/10;
 
+            }
+            if(ans%t==0){
+                return i;
+            }
+
+        }
+        return 1;
+        
+    }
 }
