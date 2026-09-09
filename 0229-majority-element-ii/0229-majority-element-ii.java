2ce210bfdfa21fe064f66da58fd253e3433921fa
Aadarsh Babu <145755350+Aadymanda@users.noreply.github.com>
2026-09-09 18:35:22 +0530

    Time: 3 ms (73.43%), Space: 52.5 MB (52.8%) - LeetHub

--- 0229-majority-element-ii/0229-majority-element-ii.java
+++ 0229-majority-element-ii/0229-majority-element-ii.java
@@ -1,19 +1,48 @@
 class Solution {
     public List<Integer> majorityElement(int[] arr) {
-        HashMap<Integer,Integer> hm=new HashMap<>();
-        List<Integer> l=new ArrayList<>();
-        int n=arr.length;
-        for(int i:arr){
-            hm.put(i,hm.getOrDefault(i,0)+1);
-        }
-        for(Map.Entry<Integer,Integer> entry:hm.entrySet()){
-            if(entry.getValue()>n/3){
-                l.add(entry.getKey());
+        List<Integer> l = new ArrayList<>();
+        int n = arr.length;
+        long first = arr[0];
+        int fq = 0;
+        long second = 1000000000;
+        int sq = 0;
+        for (int i : arr) {
+             if (first == i) {
+                fq++;
+            }  else if (second == i) {
+                sq++;
+
+            } 
+            else if (fq == 0) {
+                first = i;
+                fq=1;
+            }else if (sq == 0) {
+                second = i;
+                sq=1;
+            }else {
+                fq--;
+                sq--;
             }
+
+        }
+        int f = 0;
+        int s = 0;
+        for (int i : arr) {
+            if (i == first) {
+                f++;
+            } else if (i == second) {
+                s++;
+            }
+
+        }
+        if (f > n / 3) {
+            l.add((int) first);
+        }
+        if (s > n / 3) {
+            l.add((int) second);
         }
         return l;
-        
 
-        
     }
+
 }
