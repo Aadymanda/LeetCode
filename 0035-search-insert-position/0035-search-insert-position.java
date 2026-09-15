e2aadffe55862197854582da13a913742bfdf94f
Aadarsh Babu <145755350+Aadymanda@users.noreply.github.com>
2026-09-15 19:10:21 +0530

    Time: 0 ms (100%), Space: 44.9 MB (29.01%) - LeetHub

--- 0035-search-insert-position/0035-search-insert-position.java
+++ 0035-search-insert-position/0035-search-insert-position.java
@@ -1,31 +1,23 @@
 class Solution {
     public int searchInsert(int[] arr, int target) {
-        if(target<arr[0]){
-            return 0;
-        }
-        if(target>arr[arr.length-1]){
-            return arr.length;
-        }
+        int ans=arr.length;
         int i=0;
         int j=arr.length-1;
-        int ans=0;
-        int mid=0;
         while(i<=j){
-             mid=i+(j-i)/2;
+            int mid=i+(j-i)/2;
             if(arr[mid]==target){
-                return mid;
+                ans=mid;
+                break;
             }
-            else if(arr[mid]<target){
-               
-                i=mid+1;
-               
-            }
-            else{
+            else if(arr[mid]>target){
+                ans=mid;
                 j=mid-1;
             }
+            else{
+                i=mid+1;
+            }
         }
-        return i;
-        
+        return ans;
         
     }
 }
